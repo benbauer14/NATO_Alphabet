471ff787667b48d02e428718a1b4fc0c1c725693
Ben Bauer <ben.bauer1@gmail.com>
2021-07-20 12:13:39 -0500

    text translates

--- main.py
+++ main.py
@@ -1,27 +1,27 @@
-student_dict = {
-    "student": ["Angela", "James", "Lily"], 
-    "score": [56, 76, 98]
-}
-
-#Looping through dictionaries:
-for (key, value) in student_dict.items():
-    #Access key and value
-    pass
-
 import pandas
-student_data_frame = pandas.DataFrame(student_dict)
 
-#Loop through rows of a data frame
-for (index, row) in student_data_frame.iterrows():
-    #Access index and row
-    #Access row.student or row.score
-    pass
+alphabet = pandas.read_csv("nato_phonetic_alphabet.csv")
+alphabet_data_frame = pandas.DataFrame(alphabet)
 
-# Keyword Method with iterrows()
-# {new_key:new_value for (index, row) in df.iterrows()}
+text = input("Please enter a value to be translated using the NATO alphabet:")
+alphaList = []
 
-#TODO 1. Create a dictionary in this format:
-{"A": "Alfa", "B": "Bravo"}
+for letter in text:
+    for (index, row) in alphabet_data_frame.iterrows():
+        if(letter.upper() == row.letter):
+            alphaList.append(row.code)
+print(alphaList)
+# #Loop through rows of a data frame
+# for (index, row) in student_data_frame.iterrows():
+#     #Access index and row
+#     #Access row.student or row.score
+#     pass
 
-#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+# # Keyword Method with iterrows()
+# # {new_key:new_value for (index, row) in df.iterrows()}
+
+# #TODO 1. Create a dictionary in this format:
+# {"A": "Alfa", "B": "Bravo"}
+
+# #TODO 2. Create a list of the phonetic code words from a word that the user inputs.
 
